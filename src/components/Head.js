49b3644f2b2361370c1962d8b8d00e0ce8dc27b5
Saifal Aman <saifalaman@gmail.com>
2021-05-31 14:25:42 +0530

    first app:

---
+++ src/components/Head.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Head extends Component {
+    render() {
+        return (
+            <div class="container">
+                <h2>Panel Group</h2>
+                <div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Head;
